e7e3e18907138cd20029ac6eaefdd4a94f89dcdb
nicknazari <nhn2915@gmail.com>
2021-11-01 22:13:40 -0400

    added project class

---
+++ Project.py
@@ -0,0 +1,9 @@
+class Project:
+	def __init__(self, name, description, total_time = 0):
+		self.name = name
+		self.description = description:
+		self.total_time = total_time;
+	
+	def toJson(self):
+		out = {}
+		return 
